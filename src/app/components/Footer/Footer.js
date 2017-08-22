850d41f51900a4b80faed4deb793996e9996f7bd
Jack Bevis <jack.bevis@gmail.com>
2017-08-21 19:07:15 -0600

    footer component finished

---
+++ src/app/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = ({ handleScrollUp }) => {
+  return (
+    <footer id='footer'>
+      <a className='footer-icons github'
+         href='https://github.com/jbevis'
+         target='_blank'
+         title='GitHub'
+      </a>
+      <a className='footer-icons linkedin'
+           href='https://www.linkedin.com/in/jack-bevis-884073138/'
+           target='_blank'
+           title='Linkedin'
+      </a>
+      <a className='footer-icons twitter'
+           href='https://twitter.com/bevisjw'
+           target='_blank'
+           title='Twitter'
+      </a>
+      <a className='footer-icons contact'
+           href='mailto:jack.bevis@gmail.com'
+           title='Contact'
+      </a>
+      <button id='scroll-up'
+              type='button'
+              onClick={ () => handleScrollUp() } >
+      </button>
+      <h4 className='footer-tag'>{ '\u00A9' } Jack Bevis | 2017 </h4>
+    </footer>
+  );
+};
+
+export default Footer;

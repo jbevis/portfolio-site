a8d0dfc950209261d931afa51ea04b217f79d3cf
Jack Bevis <jack.bevis@gmail.com>
2017-08-25 13:48:54 -0600

    update skills

--- src/app/data/skillsList.js
+++ src/app/data/skillsList.js
@@ -1,68 +1,68 @@
 const skillsList = [
-	{
-		title: 'Languages',
-		skills: [
-			'JavaScript, ',
-			'HTML5, ',
-			'CSS3, ',
-			'SQL.'
-		],
-		imageClass: 'lang-icon',
-		imageAlt: 'Closing tag'
-	},
-	{
-		title: 'Frameworks/Libraries',
-		skills: [
-			'Node.js, ',
-			'React.js, ',
-			'Redux, ',
-			'jQuery, ',
-			'Express.js, ',
-			'Knex, ',
-			'postgreSQL, ',
-			'Sass/SCSS.',
-		],
-		imageClass: 'lib-icon',
-		imageAlt: 'Stacked books'
-	},
-	{
-		title: 'Testing',
-		skills: [
-			'Mocha, ',
-			'Chai, ',
-			'Enzyme, ',
-			'Jest, ',
-			'Jasmine, ',
-			'Selenium.'
-		],
-		imageClass: 'test-icon',
-		imageAlt: 'Page with checkmarks'
-	},
-	{
-		title: 'Workflow',
-		skills: [
-			'TDD, ',
-			'Agile, ',
-			'Git, ',
-			'Trello, ',
-			'Waffle.io, ',
-			'CircleCI.'
-		],
-		imageClass: 'flow-icon',
-		imageAlt: 'Flowchart'
-	},
-	{
-		title: 'Web Development',
-		skills: [
-			'WAI-ARIA, ',
-			'Semantic HTML, ',
-			'Responsive Design, ',
-			'Webpack, ',
-			'RESTful API Design.',
-		],
-		imageClass: 'dev-icon',
-		imageAlt: 'Flowchart'
-	},
+  {
+    title: 'Languages',
+    skills: [
+      'JavaScript(ES6), ',
+      'SQL, ',
+      'HTML5, ',
+      'CSS3, ',
+      'Web Standards'
+    ],
+    imageClass: 'lang-icon',
+    imageAlt: 'Closing tag'
+  },
+  {
+    title: 'Frameworks/Libraries',
+    skills: [
+      'Node.js, ',
+      'React.js, ',
+      'Redux, ',
+      'jQuery, ',
+      'Express.js, ',
+      'Knex.js.'
+    ],
+    imageClass: 'lib-icon',
+    imageAlt: 'Stacked books'
+  },
+  {
+    title: 'Tools',
+    skills: [
+      'Sass/SCSS, ',
+      'postgreSQL, ',
+      'Websockets, ',
+      'Webpack, ',
+      'D3.js, ',
+      'Victory.js, ',
+      'Leaflet.js.'
+    ],
+    imageClass: 'dev-icon',
+    imageAlt: 'wrench'
+  },
+  {
+    title: 'Test Driven Development',
+    skills: [
+      'Mocha, ',
+      'Chai, ',
+      'Enzyme, ',
+      'Jest, ',
+      'Jasmine, ',
+      'Selenium.'
+    ],
+    imageClass: 'test-icon',
+    imageAlt: 'Page with checkmarks'
+  },
+  {
+    title: 'Workflow',
+    skills: [
+      'Agile, ',
+      'Git, ',
+      'Trello, ',
+      'Waffle.io, ',
+      'CircleCI.'
+    ],
+    imageClass: 'flow-icon',
+    imageAlt: 'Flowchart'
+  }
 ]
 
 export default skillsList;
